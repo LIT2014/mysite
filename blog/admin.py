3eb0375b1e39e398aa60fdc1f49855fbaac1cc07
LIT2014 <2305602698@qq.com>
2019-01-14 17:44:17 +0800

    20190114

---
+++ blog/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import BlogType, Blog
+
+@admin.register(BlogType)
+class BlogTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type_name')
+
+    def __str__(self):
+        return self.type_name
+
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'blog_type', 'author', 'created_time', 'last_updated_time')
+
+
+    def __str__(self):
+        return "<Blog: %s>" % self.title
