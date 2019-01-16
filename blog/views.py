114f9ce113e4fa9c621776e5353bb3f9dbfbdfd1
LIT2014 <2305602698@qq.com>
2019-01-16 16:29:38 +0800

    20190116

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render_to_response, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Blog, BlogType
 
 def blog_list(request):
+
+    blogs_all_list = Blog.objects.all()
+    paginator = Paginator(blogs_all_list, 10)
+    page_num = request.GET.get('page', 1)
+    page_of_blogs = paginator.get_page(page_num)
+
     context = {}
-    context['blogs'] = Blog.objects.all()
+    context['page_of_blogs'] = page_of_blogs
+    context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blog_list.html', context)
 
 
@@ -15,6 +23,7 @@ def blog_detail(request, blog_pk):
 def blogs_with_type(request, blog_type_pk):
     context = {}
     blog_type = get_object_or_404(BlogType, pk=blog_type_pk)
-    context['blogs'] = Blog.objects.filter(blog_type=blog_type)
     context['blog_type'] = blog_type
+    context['blogs'] = Blog.objects.filter(blog_type=blog_type)
+    context['blog_types'] = BlogType.objects.all()
     return render_to_response('blog/blogs_with_type.html', context)
